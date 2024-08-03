4b06185208fcfc307cceeabd960890e330b75d11
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-08-03 10:56:54 +0530

    done

--- server/index.js
+++ server/index.js
@@ -3,23 +3,28 @@ const axios = require('axios');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001; 
+const port = 3001;
 
 const corsOptions = {
-  origin: ["https://enterprises-bot.vercel.app"], 
+  origin: ["http://localhost:3000", "https://enterprises-bot.vercel.app"],
   methods: ["GET", "POST"],
   allowedHeaders: ['Content-Type'],
-  credentials: true, 
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
-const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+const TOKEN = '6819938812:AAELgtp3sNGZNuIf0t7LYZ9_KGxBQZNTGP0';
+const GROUP_CHAT_ID =  '-1002194086845';  
+
+if (!TOKEN || !GROUP_CHAT_ID) {
+  console.error('Error: Telegram Bot Token or Group Chat ID is not defined in the environment variables.');
+  process.exit(1);
+}
 
 const getRandomWikipediaArticle = async () => {
   try {
@@ -35,13 +40,13 @@ const sendMessageToTelegram = async (message) => {
   try {
     const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
     const response = await axios.post(url, {
-      chat_id: CHAT_ID,
+      chat_id: GROUP_CHAT_ID,
       text: message,
       parse_mode: 'HTML',
     });
     return response.data;
   } catch (error) {
-    console.error('Error sending message to Telegram:', error);
+    console.error('Error sending message to Telegram:', error.response ? error.response.data : error.message);
     throw error;
   }
 };
@@ -51,15 +56,20 @@ app.get('/random-article', async (req, res) => {
     const article = await getRandomWikipediaArticle();
     res.json(article);
   } catch (error) {
+    console.error('Error fetching article:', error);
     res.status(500).send('Error fetching article');
   }
 });
 
 app.post('/send-article', async (req, res) => {
   const { title, extract, url } = req.body;
+
+  if (!title || !extract || !url) {
+    return res.status(400).send('Bad Request: Missing required fields.');
+  }
   const message = `<b>${title}</b>\n\n${extract}\n\n<a href="${url}">Read more</a>`;
   try {
-    await sendMessageToTelegram(message);
+    const result = await sendMessageToTelegram(message);
     res.send('Message sent to Telegram successfully.');
   } catch (error) {
     res.status(500).send('Error sending message to Telegram');
